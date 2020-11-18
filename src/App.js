bf67917ede3f45effea61309c3d65376661ae6e1
Kevin Iturralde <keviniturralde3@gmial.com>
2020-11-18 11:03:07 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -4,17 +4,6 @@ import beyArray from './api.js'
 import BeyContainer from './Containers/BeyContainer.js'
 import Favorites from './Containers/Favorites.js'
 
-
-// const App = () => {
-
-//   return (
-//     <div className="container" >
-//       <BeyContainer beys={beyArray} />
-//       <Favorites favBeys={beyArray.filter(bey => bey.favorite)}/>
-//     </div>
-//   );
-// };
-
 class App extends React.Component {
 state = {
   api: beyArray 
